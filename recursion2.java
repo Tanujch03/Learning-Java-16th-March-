9242a1a2e0f75b473963b3f90d15d15fe6e1ffc0
Ch Tanuj <tanujchaganti@gmail.com>
2023-04-11 19:07:01 +0530

    added

--- recursion2.java
+++ recursion2.java
@@ -3,7 +3,7 @@
 public class recursion2 {
     public static void main(String[] args)
     {
-        fun(5);
+        funrev(5);
     }
     static void fun(int n)
     {
@@ -13,6 +13,19 @@ public class recursion2 {
 
         System.out.println(n);
         fun(n-1);
+
+
+    }
+
+    static void funrev(int n)
+    {
+        if(n==0) {
+            return;
+        }
+
+        //System.out.println(n);
+        funrev(n-1);
+        System.out.println(n);
 
 
     }
